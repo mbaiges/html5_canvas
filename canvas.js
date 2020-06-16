d6a889d5ed49caec9528ae1c9f80d1202705ab10
Matias S. Baiges <mbaiges@itba.edu.ar>
2020-06-16 00:00:49 -0300

    asdasd

--- canvas.js
+++ canvas.js
@@ -1,16 +1,13 @@
-// Referencing objects
+import utils from './utils.js';
 
+// Canvas Prep
 const canvas = document.querySelector('canvas');
-
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight - 4;
-
+canvas.height = window.innerHeight;
 const c = canvas.getContext('2d');
-
 const mouse = {};
 
 // Event Listeners
-
 window.addEventListener('mousemove', (event) => {
     mouse.x = event.x;
     mouse.y = event.y;
@@ -23,270 +20,13 @@ window.addEventListener('resize', () => {
     }
 
     canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight - 4;
+    canvas.height = window.innerHeight;
 
     if (restart)
         init();
 });
 
-const maxSpeed = 6;
-
-// Utilities
-
-function randomIntFromRange(min, max) {
-    return Math.random() * (max - min) + min;
-}
-
-function distance(pos1, pos2) {
-    if (pos1 != undefined && pos1.x != undefined && pos1.y != undefined && pos2 != undefined && pos2.x != undefined && pos2.y != undefined) {
-        let xDist = pos1.x - pos2.x;
-        let yDist = pos1.y - pos2.y;
-        return Math.sqrt(Math.pow(xDist,2) + Math.pow(yDist, 2));
-    }
-    throw Error('Missing parameters');
-}
-
-/**
- * Rotates coordinate system for velocities
- *
- * Takes velocities and alters them as if the coordinate system they're on was rotated
- *
- * @param  Object | velocity | The velocity of an individual particle
- * @param  Float  | angle    | The angle of collision between two objects in radians
- * @return Object | The altered x and y velocities after the coordinate system has been rotated
- */
-
-function rotate(velocity, angle) {
-    const rotatedVelocities = {
-        x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
-        y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
-    };
-
-    return rotatedVelocities;
-}
-
-/**
- * Swaps out two colliding particles' x and y velocities after running through
- * an elastic collision reaction equation
- *
- * @param  Object | particle      | A particle object with x and y coordinates, plus velocity
- * @param  Object | otherParticle | A particle object with x and y coordinates, plus velocity
- * @return Null | Does not return a value
- */
-
-function resolveCollision(particle, otherParticle) {
-    const xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
-    const yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
-
-    const xDist = otherParticle.x - particle.x;
-    const yDist = otherParticle.y - particle.y;
-
-    // Prevent accidental overlap of particles
-    if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
-
-        // Grab angle between the two colliding particles
-        const angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
-
-        // Store mass in var for better readability in collision equation
-        const m1 = particle.mass;
-        const m2 = otherParticle.mass;
-
-        // Velocity before equation
-        const u1 = rotate(particle.velocity, angle);
-        const u2 = rotate(otherParticle.velocity, angle);
-
-        // Velocity after 1d collision equation
-        const v1 = { x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2), y: u1.y };
-        const v2 = { x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2), y: u2.y };
-
-        // Final velocity after rotating axis back to original location
-        const vFinal1 = rotate(v1, -angle);
-        const vFinal2 = rotate(v2, -angle);
-
-        // Swap particle velocities for realistic bounce effect
-        particle.velocity.x = vFinal1.x;
-        particle.velocity.y = vFinal1.y;
-
-        otherParticle.velocity.x = vFinal2.x;
-        otherParticle.velocity.y = vFinal2.y;
-    }
-}
-
-function randomColor(colors) {
-    return colors[Math.floor(Math.random() * colors.length)];
-}
-
-// Implementation
-
-const opacityIncreaseRate = 0.02;
-const opacityDecreaseRate = 0.04;
-const minOpacity = 0;
-const maxOpacity = 0.3;
-const friction = 0.995;
-
-const mouseHighlightParticleRadius = 100;
-
-class Particle {
-    constructor(x, y, radius, color) {
-        this.x = x;
-        this.y = y;
-        this.velocity = {
-            x: randomIntFromRange(-2.5, 2.5),
-            y: randomIntFromRange(-2.5, 2.5)
-        }
-        this.acceleration = {
-            x: 0,
-            y: 0
-        }
-        this.radius = radius;
-        this.color = color;
-        this.mass = 1;
-        this.opacity = minOpacity;
-    }
-
-    draw() {
-        c.beginPath();
-        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        c.save();
-        c.globalAlpha = this.opacity;
-        c.fillStyle = this.color;
-        c.fill();
-        c.restore();
-        c.strokeStyle = this.color;
-        c.stroke();
-        c.closePath();
-    }
-
-    update(particles) {
-        this.draw();
-
-        for (let i = 0; i < particles.length; i++) {
-            if (this === particles[i]) continue;
-            if (distance({x: this.x, y: this.y}, {x: particles[i].x, y: particles[i].y}) - (this.radius + particles[i].radius) < 0) {
-                resolveCollision(this, particles[i]);
-            }
-        }
-
-        if (this.x - this.radius <= 0 || this.x + this.radius >= canvas.width) {
-            this.velocity.x = -this.velocity.x;
-        }
-
-        if (this.y - this.radius <= 0 || this.y + this.radius >= canvas.height) {
-            this.velocity.y = -this.velocity.y;
-        }
-
-        // mouse collision detection
-
-        if (mouse.x && mouse.y) {
-            if (distance({x: this.x, y: this.y}, {x: mouse.x, y: mouse.y}) < mouseHighlightParticleRadius && (this.opacity + opacityIncreaseRate) <= maxOpacity) {
-                this.opacity += opacityIncreaseRate;
-            }
-            else if ((this.opacity - opacityDecreaseRate) >= minOpacity) {
-                this.opacity -= opacityDecreaseRate;
-            }
-        }
-
-        if (this.acceleration.x != 0 && Math.abs(this.velocity.x + this.acceleration.x) < maxSpeed) {
-            this.velocity.x += this.acceleration.x;
-            this.acceleration.x = 0;
-        }
-
-        if (this.acceleration.y != 0 && Math.abs(this.velocity.y + this.acceleration.y) < maxSpeed) {
-            this.velocity.y += this.acceleration.y;
-            this.acceleration.y = 0;
-        }
-    
-        this.x += this.velocity.x;
-        this.y += this.velocity.y;
-
-        this.velocity.x *= friction;
-        this.velocity.y *= friction;
-    }
-}
-
-const colors = [
-    '#2185C5',
-    '#7ECEFD',
-    '#FF7F66'
-];
-
-const total_particles = 400;
-
-let particles = [];
-let my_particle;
-
-function init() {
-    particles = [];
-
-    for (let i = 0; i < total_particles; i++) {
-        const radius = 15;
-        let x = randomIntFromRange(radius, canvas.width - radius);
-        let y = randomIntFromRange(radius, canvas.height - radius);
-        const color = randomColor(colors);
-
-        if (x < radius || x > canvas.width - radius)
-            console.log("Error");
-
-        if (i !== 0) {
-            for (let j = 0; j < particles.length; j++) {
-                if (distance({x, y}, {x: particles[j].x, y: particles[j].y}) - (radius + particles[j].radius) < 0) {
-                    x = randomIntFromRange(radius, canvas.width - radius);
-                    y = randomIntFromRange(radius, canvas.height - radius);
-                    
-                    j = -1;
-                }
-            }
-        }
-
-        particles.push(new Particle(x, y, radius, color));
-    }
-
-    my_particle = particles[0];
-    my_particle.color = 'green';
-}
-
-const keys = {};
-
-function animate() {
-    requestAnimationFrame(animate);
-    c.clearRect(0, 0, canvas.width, canvas.height);
-
-    Object.entries(keys).forEach(entry => {
-        if (entry[1]) {
-            switch(entry[0]) {
-                case 'ArrowUp':
-                    if (my_particle.velocity.y - 1 >= - maxSpeed)
-                        my_particle.velocity.y += -1;
-                    // my_particle.acceleration.y = -1;
-                    break;
-                case 'ArrowDown':
-                    if (my_particle.velocity.y + 1 <= maxSpeed )
-                        my_particle.velocity.y += 1;
-                    // my_particle.acceleration.y = 1;
-                    break;
-                case 'ArrowLeft':
-                    if (my_particle.velocity.x - 1 >= - maxSpeed)
-                        my_particle.velocity.x += -1;
-                    // my_particle.acceleration.x = -1;
-                    break;
-                case 'ArrowRight':
-                    if (my_particle.velocity.x + 1 <= maxSpeed)
-                        my_particle.velocity.x += 1;
-                    // my_particle.acceleration.x = 1;                        
-                    break;
-                case 'r':
-                    init();
-                    break;    
-            }
-        }
-    });
-
-    particles.forEach(particle => {
-        particle.update(particles);
-    })
-}
-
-
+let keys = {};
 
 window.addEventListener('keydown', (event) => {
     const key = event.key;
@@ -297,6 +37,265 @@ window.addEventListener('keyup', (event) => {
     const key = event.key;
     keys[key] = false;
 });
+
+// Models 
+
+const closeBoidsRange = 1000;
+const boidTurnSpeed = 0.1;
+const factors = {
+    separation: 1,
+    cohesion: 1,
+    alignment: 1
+};
+
+class Boid{
+    constructor(x, y, midlen, speed, color){
+        this.position = {
+            x: x,
+            y: y
+        }
+        this.headAlpha = Math.PI / 3;
+        this.midlen = midlen;
+        this.speed = speed;
+        this.angle = utils.randomFloatFromRange(0, 2 * Math.PI);
+        this.calculateVelocity();
+        this.calculateShape();
+        this.color = color;
+    }
+    
+    draw(){
+        c.beginPath();
+        c.fillStyle = this.color;
+        c.moveTo(this.shape.head.x, this.shape.head.y);
+        c.lineTo(this.shape.leftTail.x, this.shape.leftTail.y);
+        c.lineTo(this.shape.tailBreak.x, this.shape.tailBreak.y);
+        c.lineTo(this.shape.rightTail.x, this.shape.rightTail.y);
+        c.closePath();
+        c.fill();
+    }
+    
+    update(boids){
+
+        const closestBoids = this.findBoidsInRange(boids)
+
+        let variation;
+
+        if(closestBoids.length > 0){
+            const sepVector = this.separation(closestBoids);
+            const aliVector = this.alignment(closestBoids);
+            const coheVector = this.cohesion(closestBoids);
+            //console.log(sepVector, aliVector, coheVector);
+            variation = this.parseMovement(sepVector, aliVector, coheVector);
+        }else{
+            variation = 0;
+        }
+
+        if ( ((this.angle + variation) % (2 * Math.PI)) < 0 ) {
+            this.angle += 2 * Math.PI;
+        }
+        this.angle = (this.angle + variation) % (2 * Math.PI);
+        
+        this.calculateVelocity();
+
+        if (this.velocity) {
+            if ((this.position.x + this.velocity.x) % canvas.width < 0)
+                this.position.x += canvas.width;
+            this.position.x = (this.position.x + this.velocity.x) % canvas.width;
+            if ((this.position.y + this.velocity.y) % canvas.height < 0)
+                this.position.y += canvas.height;
+            this.position.y = (this.position.y + this.velocity.y) % canvas.height;
+        }
+         
+        this.calculateShape();
+        this.draw();
+    }
+
+    findBoidsInRange(boids){
+        let closest = [];
+        boids.forEach(boid => {
+            if (this !== boid && utils.distance({x: this.position.x, y: this.position.y}, {x: boid.position.x, y: boid.position.y}) < closeBoidsRange) {
+                closest.push(boid);
+            }
+        });
+        return closest;
+    }
+
+    parseMovement(sepVector, aliVector, coheVector) {
+        let totalVectorX = factors.separation * sepVector.x + factors.alignment * aliVector.x + factors.cohesion * coheVector.x;
+        let totalVectorY = factors.separation * sepVector.y + factors.alignment * aliVector.y + factors.cohesion * coheVector.y;
+
+        let desiredAngle;
+        //console.log(totalVectorX, totalVectorY);
+        if (totalVectorX !== 0) {
+            desiredAngle = Math.atan(totalVectorY / totalVectorX);
+        }
+        else {
+            desiredAngle = (Math.PI / 2) * Math.sign(totalVectorY);
+        }
+        if (desiredAngle < 0){
+            desiredAngle += 2 * Math.PI;
+        }
+        console.log(desiredAngle, this.angle) 
+        let ans;
+        if( ( ( (this.angle - desiredAngle) > 0) && ( (this.angle - desiredAngle) < Math.PI) ) || ( ( (desiredAngle - this.angle) > 0 ) && ((desiredAngle - this.angle) > Math.PI) ) ){
+            ans = boidTurnSpeed;
+        }else {
+            ans = -boidTurnSpeed;
+        }
+        
+        console.log(ans);;
+        return ans;
+    }
+
+    separation(closestBoids) {
+        let total = {
+            x: 0,
+            y: 0
+        }
+
+        closestBoids.forEach(boid => {
+            total.x += (boid.position.x - this.position.x);
+            total.y += (boid.position.y - this.position.y);
+        })
+
+        const size = Math.sqrt(Math.pow(total.x, 2) + Math.pow(total.x, 2));
+
+        total.x /= size;
+        total.y /= size;
+
+        return total;
+    }
+    
+    alignment(closestBoids){
+        let total = {
+            x: 0,
+            y: 0
+        };
+
+        closestBoids.forEach(boid => {
+            total.x += boid.velocity.x;
+            total.y += boid.velocity.y;
+        });
+
+        const size = Math.sqrt(Math.pow(total.x, 2) + Math.pow(total.y, 2));
+
+        total.x /= size;
+        total.y /= size;
+
+        return total;
+    }
+    
+    cohesion(closestBoids) {
+        let prom = {
+            x: 0,
+            y: 0
+        }
+
+        closestBoids.forEach(boid => {
+            prom.x += boid.position.x;
+            prom.y += boid.position.y;
+        })
+
+        prom.x /= closestBoids.length;
+        prom.y /= closestBoids.length;
+        
+        let vectorToCenter = {
+            x: prom.x - this.position.x,
+            y: prom.y - this.position.y
+        }
+
+        const size = Math.sqrt(Math.pow(vectorToCenter.x, 2) + Math.pow(vectorToCenter.y, 2));
+
+        vectorToCenter.x /= size;
+        vectorToCenter.y /= size;
+
+        //console.log('asd');
+        //console.log(vectorToCenter);
+        
+        return vectorToCenter;
+    }
+
+    calculateVelocity(){
+        if (!this.velocity)
+            this.velocity = {};
+        this.velocity = {
+            x: this.speed * Math.cos(this.angle),
+            y: this.speed * Math.sin(this.angle)
+        }
+    }
+
+    calculateShape(){
+        if (!this.shape)
+            this.shape = {};
+
+        this.shape.head = {
+            x: this.position.x + Math.cos(this.angle) * this.midlen,
+            y: this.position.y + Math.sin(this.angle) * this.midlen
+        };
+
+        this.shape.tailBreak = {
+            x: this.position.x + Math.cos(this.angle) * this.midlen /4,
+            y: this.position.y + Math.sin(this.angle) * this.midlen /4
+        };
+
+        const rightTailAngle = this.angle + (Math.PI / 2);
+        this.shape.rightTail = {
+            x: this.position.x + Math.cos(rightTailAngle) * (this.midlen/2),
+            y: this.position.y + Math.sin(rightTailAngle) * (this.midlen/2) 
+        };
+
+        const leftTailAngle = this.angle - (Math.PI / 2);
+        this.shape.leftTail = {
+            x: this.position.x + Math.cos(leftTailAngle) * (this.midlen/2),
+            y: this.position.y + Math.sin(leftTailAngle) * (this.midlen/2) 
+        }
+
+        //console.log(this.shape.head, this.shape.leftTail, this.shape.rightTail);
+
+    }
+}
+
+// Implementation
+
+const colors = [
+    '#2185C5',
+    '#7ECEFD',
+    '#FF7F66'
+];
+
+var boids = [] 
+var total_boids = 100;
+
+function init() {
+    boids = [];
+    for (let i = 0; i < total_boids; i++) {
+        const speed = 2;
+        const midlen = 20;
+        const color = utils.randomColor(colors);
+        const x = utils.randomIntFromRange(midlen, canvas.width - midlen);
+        const y = utils.randomIntFromRange(midlen, canvas.height - midlen)
+        boids.push(new Boid(x, y, midlen, speed, color));
+    }
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+
+    Object.entries(keys).forEach(entry => {
+        if (entry[1]) {
+            switch(entry[0]) {
+                case 'r':
+                    init();
+                    break;    
+            }
+        }
+    });
+
+    boids.forEach(boid => {
+        boid.update(boids);
+    })
+}
 
 init();
 animate();
